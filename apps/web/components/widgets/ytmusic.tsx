4a35fc7735060fb046ecd468719d78daa28d006d
flatypus [mac] <yhc3141@gmail.com>
2023-12-22 20:47:50 -0800

    fix: null case

--- apps/web/components/widgets/ytmusic.tsx
+++ apps/web/components/widgets/ytmusic.tsx
@@ -61,26 +61,37 @@ export default function Playing(): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps -- This is fine
   }, [time]);
 
+  if (!currentlyPlaying) {
+    return (
+      <div className="flex h-[32px] w-full flex-row place-items-center items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg bg-white bg-opacity-80 p-2 text-[12px] shadow-lg">
+        <Image alt="Music" height={16} src="/icons/music.png" width={16} />
+        <p className="max-w-[90%] truncate text-black text-opacity-40">
+          Hinson isn&apos;t listening to anything right now.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-[80px] w-full flex-row items-center overflow-hidden rounded-lg bg-white bg-opacity-80 p-2 text-[8px] shadow-lg">
       <Image
-        alt={currentlyPlaying?.title ?? ""}
+        alt={currentlyPlaying.title}
         className="mr-2 h-full rounded-md"
         height={64}
-        src={currentlyPlaying?.thumbnail ?? ""}
+        src={currentlyPlaying.thumbnail}
         width={64}
       />
       <div className="flex h-full w-full flex-col place-items-center justify-center">
         <a
           className="line-clamp-2 max-w-[90%] font-semibold text-black hover:underline"
-          href={currentlyPlaying?.url}
+          href={currentlyPlaying.url}
           rel="noreferrer"
           target="_blank"
         >
           <span className="whitespace-nowrap font-normal text-black">
             Hinson is now listening to:&nbsp;
           </span>
-          {currentlyPlaying?.artist} - {currentlyPlaying?.title}
+          {currentlyPlaying.artist} - {currentlyPlaying.title}
         </a>
         <div className="mt-1 h-[2px] w-full rounded-lg bg-gray-400">
           <div
@@ -88,8 +99,8 @@ export default function Playing(): JSX.Element {
             style={{
               width: `${Math.min(
                 100,
-                (100 * (time - (currentlyPlaying?.started ?? 0))) /
-                  (currentlyPlaying?.duration_s ?? 1),
+                (100 * (time - currentlyPlaying.started)) /
+                  currentlyPlaying.duration_s,
               )}%`,
             }}
           >
@@ -100,12 +111,12 @@ export default function Playing(): JSX.Element {
           <div>
             {formatSeconds(
               Math.min(
-                currentlyPlaying?.duration_s ?? 0,
-                time - (currentlyPlaying?.started ?? 0),
+                currentlyPlaying.duration_s,
+                time - currentlyPlaying.started,
               ),
             )}
           </div>
-          <div>{formatSeconds(currentlyPlaying?.duration_s ?? 0)}</div>
+          <div>{formatSeconds(currentlyPlaying.duration_s)}</div>
         </div>
       </div>
     </div>
